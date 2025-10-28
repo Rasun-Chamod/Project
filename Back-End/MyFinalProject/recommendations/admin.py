0f7667a27487a6112dcbad4a4754a667c176c959
Rasun-Chamod <w.a.r.chamod@gmail.com>
2025-10-28 10:15:21 +0530

    Asjusting the imports for Recommendations

--- Back-End/MyFinalProject/recommendations/admin.py
+++ Back-End/MyFinalProject/recommendations/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import RecommendationCache
 
-# Register your models here.
+@admin.register(RecommendationCache)
+class RecommendationCacheAdmin(admin.ModelAdmin):
+    list_display = ("user", "recommendation_type", "generated_at", "expires_at")
+    list_filter = ("recommendation_type",)
+    search_fields = ("user__username",)
