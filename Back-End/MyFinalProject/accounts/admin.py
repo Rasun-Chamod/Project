a606320f427543606db9ee230202eb8db398112d
Rasun-Chamod <w.a.r.chamod@gmail.com>
2025-10-28 10:06:28 +0530

    Asjusting the imports for accounts app

--- Back-End/MyFinalProject/accounts/admin.py
+++ Back-End/MyFinalProject/accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Profile
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Extra Info", {"fields": ("email",)}),
+    )
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "favourite_genre", "favourite_platform", "created_at")
