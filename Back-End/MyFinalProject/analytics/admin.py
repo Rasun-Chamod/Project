2b9ffea42b0c1a40f0de2b0208f27bbaa884352c
Rasun-Chamod <w.a.r.chamod@gmail.com>
2025-10-28 10:12:51 +0530

    Asjusting the imports for Analytics

--- Back-End/MyFinalProject/analytics/admin.py
+++ Back-End/MyFinalProject/analytics/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import UserAggregate
 
-# Register your models here.
+@admin.register(UserAggregate)
+class UserAggregateAdmin(admin.ModelAdmin):
+    list_display = ("user", "period", "period_start", "total_watch_minutes", "total_play_minutes")
+    list_filter = ("period", "period_start")
