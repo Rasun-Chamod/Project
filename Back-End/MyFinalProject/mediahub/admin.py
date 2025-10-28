16ccdcf9946ead1d931c57395c66a7239b2ace6e
Rasun-Chamod <w.a.r.chamod@gmail.com>
2025-10-28 10:07:56 +0530

    Asjusting the imports for mediahub

--- Back-End/MyFinalProject/mediahub/admin.py
+++ Back-End/MyFinalProject/mediahub/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Genre, Platform, Tag, MediaItem
 
-# Register your models here.
+admin.site.register(Genre)
+admin.site.register(Platform)
+admin.site.register(Tag)
+
+@admin.register(MediaItem)
+class MediaItemAdmin(admin.ModelAdmin):
+    list_display = ("title", "media_type", "is_active", "created_at")
+    list_filter = ("media_type", "is_active", "genres")
+    search_fields = ("title", "slug", "description")
