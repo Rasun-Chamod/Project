055a841208edfd18e27766760218814c51d8de2f
Rasun-Chamod <w.a.r.chamod@gmail.com>
2025-10-28 11:36:11 +0530

    Addin CORS_ALLOWED_ORIGINS to settings.py

--- Back-End/MyFinalProject/MyFinalProject/settings.py
+++ Back-End/MyFinalProject/MyFinalProject/settings.py
@@ -48,6 +48,11 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
