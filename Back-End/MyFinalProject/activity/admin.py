7863ddda50d214eb6e1563e21dd20d56889a8bd5
Rasun-Chamod <w.a.r.chamod@gmail.com>
2025-10-28 10:10:18 +0530

    Asjusting the imports for Activiry app

--- Back-End/MyFinalProject/activity/admin.py
+++ Back-End/MyFinalProject/activity/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserActivity
 
-# Register your models here.
+@admin.register(UserActivity)
+class UserActivityAdmin(admin.ModelAdmin):
+    list_display = ("user", "mediaitem", "activity_type", "created_at")
+    list_filter = ("activity_type", "created_at")
+    search_fields = ("user__username", "mediaitem__title")
