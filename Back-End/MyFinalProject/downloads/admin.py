2cfe542646068e68f2ba039704890dcf9a70ce5c
Rasun-Chamod <w.a.r.chamod@gmail.com>
2025-10-28 10:14:08 +0530

    Asjusting the imports for downloads

--- Back-End/MyFinalProject/downloads/admin.py
+++ Back-End/MyFinalProject/downloads/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import DownloadLink
 
-# Register your models here.
+@admin.register(DownloadLink)
+class DownloadLinkAdmin(admin.ModelAdmin):
+    list_display = ("mediaitem", "user", "download_token", "expires_at", "download_count")
+    list_filter = ("expires_at",)
+    search_fields = ("mediaitem__title", "user__username", "download_token")
